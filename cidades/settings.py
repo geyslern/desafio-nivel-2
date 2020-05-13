2a0abc1c4a0775396ee52f44a0d74e2204aefaa5
Geysler Niclevicz <geyslern@gmail.com>
2020-05-13 18:15:19 -0300

    Configuração da variável SECRET_KEY

--- cidades/settings.py
+++ cidades/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&5ipw49d@ntu81(*1zdd=afj42o!&u0084oa+dr6u=_6$b91w3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
